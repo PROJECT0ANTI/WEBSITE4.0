b4f414352bf33865ef09cc192ebedce4db4e0c38
Kunal Saxena <161448306+kunalsaxena0411@users.noreply.github.com>
2026-01-08 03:21:09 +0530

    Fix import statement for API_BASE

--- src/Components/Joblist/JobListing.jsx
+++ src/Components/Joblist/JobListing.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./JobListing.css";
-import { API_BASE } from "../../config/api";
+import API_BASE from "../../config/api";
 
 export default function JobListing() {
   const [jobs, setJobs] = useState([]);
